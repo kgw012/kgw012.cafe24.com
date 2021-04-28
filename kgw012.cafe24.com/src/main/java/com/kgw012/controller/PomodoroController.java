1359b4e306fdc21780baebb22add67f654d5f948
kgw012 <kgw012@gmail.com>
2021-04-28 13:15:39 +0900

    210428 / pomodoro timer

--- kgw012.cafe24.com/src/main/java/com/kgw012/controller/PomodoroController.java
+++ kgw012.cafe24.com/src/main/java/com/kgw012/controller/PomodoroController.java
@@ -5,11 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PortfolioController {
+public class PomodoroController {
 	
-
-	@GetMapping(value = "/resume.do")
+	@GetMapping(value = "/pomodoro.do")
 	public String openIndexPage(Model model) {
-		return "resume/resume";
+		return "projects/pomodoro_timer/pomodoro_timer";
 	}
 }
