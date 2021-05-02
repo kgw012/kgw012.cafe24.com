fe19256c8d773f36b842f11b0b5634513a9bddde
kgw012 <kgw012@gmail.com>
2021-05-02 15:10:10 +0900

    rebuild commit

---
+++ src/main/java/com/aboutkgw/AboutKgwApplication.java
@@ -0,0 +1,13 @@
+package com.aboutkgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutKgwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AboutKgwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aboutkgw/AboutKgwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aboutkgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutKgwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
